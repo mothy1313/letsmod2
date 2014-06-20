76a03e9342b5949ce8e0f6eb41c722f14e699289
moth1313 <mothy1313@gmail.com>
2014-06-20 21:00:36 +0100

    Create mod core following Pahimar Let’s Mod Reboot episode 3

---
+++ src/main/scala/com/mothy/letsmod2/Letsmod2.scala
@@ -0,0 +1,31 @@
+package com.mothy.letsmod2
+
+
+import cpw.mods.fml.common.Mod
+import cpw.mods.fml.common.Mod.EventHandler
+import cpw.mods.fml.common.event.{FMLPostInitializationEvent, FMLPreInitializationEvent, FMLInitializationEvent}
+
+@Mod(name = "Lets Mod 2", modid = "letsmod2", version = "1.7.2-1.0", modLanguage = "scala")
+object Letsmod2 {
+
+  @EventHandler
+  def preInit(event: FMLPreInitializationEvent){
+
+
+  }
+
+  @EventHandler
+  def init(event: FMLInitializationEvent) {
+
+
+  }
+
+  @EventHandler
+  def postInit(event: FMLPostInitializationEvent){
+
+
+  }
+}
+
+
+
