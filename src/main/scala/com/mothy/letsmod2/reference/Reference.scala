03460c4cad9b60d5de9ba56da9b2cd67a7ef9a6c
mothy1313 <mothy1313@gmail.com>
2014-07-06 20:53:51 +0100

    Update forge version in Reference object

--- src/main/scala/com/mothy/letsmod2/reference/Reference.scala
+++ src/main/scala/com/mothy/letsmod2/reference/Reference.scala
@@ -5,7 +5,7 @@ object Reference {
 
   final val ModId = "letsmod2"
   final val ModName = "Lets Mod 2"
-  final val Version = "1.7.2-1.0"
+  final val Version = "1.7.10-1.0"
   final val ModLang = "scala"
   final val ClientProxyClass = "com.mothy.letsmod2.proxy.ClientProxy"
   final val ServerProxyClass = "com.mothy.letsmod2.proxy.ServerProxy"
